b378188e476862280cc5793e8361bf3ce65cdd00
bezcontrol <oleksii.baklanov@nure.ua>
2018-12-08 21:16:11 +0200

    cleaning class

--- profITexercises/src/main/java/ua/kh/profit/baklanov/content/dao/ConnectionFactoryImplementation.java
+++ profITexercises/src/main/java/ua/kh/profit/baklanov/content/dao/ConnectionFactoryImplementation.java
@@ -1,39 +1,29 @@
 package ua.kh.profit.baklanov.content.dao;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSourceFactory;
 
 public class ConnectionFactoryImplementation implements ConnectionFactory{
-    /**
-     * Returns new Pooled {@link DataSource} implementation
-     *
-     * @param poolProperties pool properties
-     * @return new Pooled {@link DataSource} implementation
-     * @throws SQLException
-     */
-    public static DataSource createDataSource(Properties poolProperties) throws SQLException {
-        try {
-            return BasicDataSourceFactory.createDataSource(poolProperties);
-        } catch (Exception e) {
-            throw new SQLException(e);
-        }
-    }
+    Properties properties;
 
     @Override
     public Connection createConnection() throws Exception {
+        properties=new Properties();
         try
         {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            InputStream in = new FileInputStream(DaoFactory.class.getResource("/dbcp.properties").getPath());
+            properties.load(in);
+            Class.forName(properties.getProperty("driverClassName")).newInstance();
         }
         catch (ClassNotFoundException e){
             throw new RuntimeException(e);
         }
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/myschema","root","root");
+            return DriverManager.getConnection(properties.getProperty("url"),properties.getProperty("username"),properties.getProperty("password"));
         } catch (SQLException e) {
             throw new Exception(e);
         }
